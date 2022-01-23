48d14c5829e5afbce623edb33b953b4d80eb0c29
InnaMih212 <miheikinainna212@gmail.com>
2022-01-23 22:06:09 +0300

    fix

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,16 +3,23 @@ import s from './Dialogs.module.css';
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 import {Field, reduxForm} from "redux-form";
+import {Redirect} from "react-router-dom";
+import {Textarea} from "../common/FormsControls/FormsControls";
+import {maxLengthCreator, required} from "../../utils/validators/validators";
+import AddMessageForm from "./AddMessageForm/AddMessageForm";
 
 const Dialogs = (props) => {
     let state = props.dialogsPage;
 
     let dialogsElements = state.dialogs.map( d => <DialogItem name={d.name} key={d.id} id={d.id} /> );
     let messagesElements = state.messages.map( m => <Message message={m.message} key={m.id}/> );
+    let newMessageBody = state.newMessageBody;
 
     let addNewMessage = (values) => {
        props.sendMessage (values.newMessageBody);/*такое же как и name*/
     };
+
+    if(!props.isAuth) return <Redirect to={"/login"} />;
 
     return (
         <div className={s.dialogs}>
@@ -21,23 +28,11 @@ const Dialogs = (props) => {
         </div>
             <div className={s.messages}>
                <div>{ messagesElements }</div> {/*сообщения*/}
-                <AddMessageFormRedux  onSubmit={addNewMessage}/>
+                <AddMessageForm  onSubmit={addNewMessage}/>
             </div>
         </div>
     )
 }
-
-const AddMessageForm = (props) => {
-   return (
-    <form onSubmit={props.handleSubmit}>
-        <div>
-        <Field component="textarea" name="newMessageBody" placeholder="Enter your message" />
-        </div>
-        <div><button>Send</button></div>
-    </form>
-)
-}
-const AddMessageFormRedux = reduxForm({form: "dialogAddMessageForm"})(AddMessageForm);
 
 export default Dialogs;
 
--- src/components/common/FormsControls/FormsControls.js
+++ src/components/common/FormsControls/FormsControls.js
@@ -3,12 +3,14 @@ import React from "react";
 import styles from "./FormsControls.module.css";
 
 export const Textarea = ({input,meta, ...props}) => {
+
+    const hasError = meta.touched && meta.error;
     return (
-        <div className={styles.formControl + " " + styles.error}>
+        <div className={styles.formControl + " " + (hasError ? styles.error : " ")}>
             <div>
-            <textarea {...props.input} {...props}/>
+            <textarea {...props.input} {...props} />
             </div>
-            {meta.touched && meta.error && <span>"some_error"</span> }
+            { hasError && <span>{meta.error}</span> }
         </div>
     )
 }
