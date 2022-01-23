27d14789281c47f7719cf4e97a5fc498c0615726
InnaMih212 <miheikinainna212@gmail.com>
2022-01-23 22:22:24 +0300

    fix

--- src/components/common/FormsControls/FormsControls.js
+++ src/components/common/FormsControls/FormsControls.js
@@ -1,16 +1,22 @@
-
 import React from "react";
 import styles from "./FormsControls.module.css";
 
-export const Textarea = ({input,meta, ...props}) => {
-
+const FormControl = ({input,meta,child, ...props}) => {
     const hasError = meta.touched && meta.error;
     return (
         <div className={styles.formControl + " " + (hasError ? styles.error : " ")}>
             <div>
-            <textarea {...props.input} {...props} />
+                {props.child}
             </div>
             { hasError && <span>{meta.error}</span> }
         </div>
+    )
+}
+
+export const Textarea = ({input,meta, ...props}) => {
+    return <FormControl {...props}> <textarea {...input} {...props} /></FormControl>
+}
+export const Input = ({input,meta, ...props}) => {
+    return <FormControl {...props}> <input {...input} {...props} /></FormControl>
     )
 }
