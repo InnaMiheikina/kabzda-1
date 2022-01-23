873b0c8034a812584a2b8b0d8dfcea9b61ed7174
InnaMih212 <miheikinainna212@gmail.com>
2022-01-23 15:34:08 +0300

    fix

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 import {Field, reduxForm} from "redux-form";
+import {required} from "../../../utils/validators/validators";
 
 
 
     let AddNewPostForm = (props) => {
         return <form onSubmit={props.handleSubmit}>
             <div>
-                <Field name="newPostText" component="textarea" />
+                <Field name="newPostText" component="textarea"
+                       validate={[required, maxLength15, minLength2]}  />
             </div>
             <div>
                 <button>Add post</button>
